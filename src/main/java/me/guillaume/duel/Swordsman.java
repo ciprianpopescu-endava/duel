89066237a85f7becea2207cc9fa3bddc46fba271
Cipri Popescu <cipri.popescu@gmail.com>
2020-02-21 13:52:30 +0200

    second test

--- src/main/java/me/guillaume/duel/Swordsman.java
+++ src/main/java/me/guillaume/duel/Swordsman.java
@@ -1,7 +1,10 @@
 package me.guillaume.duel;
 
+import java.util.Optional;
+
 public class Swordsman {
     private int hitPoints;
+    private String equipment;
 
     public int getHitPoints() {
         return hitPoints;
@@ -22,11 +25,45 @@ public class Swordsman {
     }
 
     public void engage(Viking viking) {
+        int axeStrike=0;
+        int strikeNo=1;
         do{
-            viking.setHitPoints(viking.hitPoints()-5);
-            this.setHitPoints(hitPoints-6);
+            if(axeStrike==3)
+                this.equip(null);
+
+            //swordsman attacks viking
+            swordsmanVsViking(viking, strikeNo);
+
+            //viking attacks swordsman
+            axeStrike = vikingVsSwordsman(axeStrike, strikeNo);
+            strikeNo++;
         }while ((viking.hitPoints()>0)&&(hitPoints>0));
 
+    }
+
+    private int vikingVsSwordsman(int axeStrike, int strikeNo) {
+        if(Optional.ofNullable(getEquipment()).isPresent()){
+            if(strikeNo%2==0){
+                this.setHitPoints(hitPoints-6);
+            }
+            else
+            {
+                axeStrike++;
+            }
+        }
+        else
+            this.setHitPoints(hitPoints-6);
+        return axeStrike;
+    }
+
+    private void swordsmanVsViking(Viking viking, int strikeNo) {
+        if(Optional.ofNullable(viking.getEquipment()).isPresent()){
+            if(strikeNo%2==0){
+               viking.setHitPoints(viking.hitPoints()-5);
+            }
+        }
+        else
+            viking.setHitPoints(viking.hitPoints()-5);
     }
 
     public void engage(Highlander highlander) {
@@ -37,7 +74,12 @@ public class Swordsman {
     }
 
     public Swordsman equip(String buckler) {
-    return this;
-
+        this.equipment=buckler;
+        return  this;
     }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
 }
--- src/main/java/me/guillaume/duel/Viking.java
+++ src/main/java/me/guillaume/duel/Viking.java
@@ -2,16 +2,12 @@ package me.guillaume.duel;
 
 public class Viking {
     private int hitPoints;
+    private String equipment;
 
 
     public void setHitPoints(int hitPoints) {
             this.hitPoints =Math.max(hitPoints, 0);
     }
-
-    public Viking equip(String buckler) {
-        return this;
-    }
-
 
     public Viking () {
         this.setHitPoints(120);
@@ -20,4 +16,14 @@ public class Viking {
     public int hitPoints() {
         return hitPoints;
     }
+
+    public Viking equip(String buckler) {
+        this.equipment=buckler;
+        return  this;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
 }
